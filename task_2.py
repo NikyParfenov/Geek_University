db5b8bbc17ac0f03d96db18c16a6dda443d00387
Nikita Parfenov <63195531+NikyParfenov@users.noreply.github.com>
2020-04-09 18:46:29 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -12,5 +12,5 @@ while True:
 seconds = user_seconds % 60
 minutes = user_seconds // 60 % 60
 hours = user_seconds // 3600
-print(f'\nВремя (в формате hh:mm:ss): {hours}:{minutes}:{seconds}')
+print(f'\nВремя (в формате hh:mm:ss): {hours:>02}:{minutes:>02}:{seconds:>02}')
 print("Внимание! Количество часов превышает 1 сутки!") if hours > 24 else None
